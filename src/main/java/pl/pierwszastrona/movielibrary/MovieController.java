c63f98a55938dc01147cdba7f0020bfb1770542c
unknown <karolina.regula15@gmail.com>
2023-07-17 20:47:58 +0200

    final version

--- src/main/java/pl/pierwszastrona/movielibrary/MovieController.java
+++ src/main/java/pl/pierwszastrona/movielibrary/MovieController.java
@@ -1,29 +1,73 @@
 package pl.pierwszastrona.movielibrary;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
 
-    @GetMapping("/test")
-    public int test() {
-    return 1;
-    }
-    @GetMapping("/movies")
-    public List<Movie> getAll(){
+    //    @GetMapping("/test")
+//    public String test() {
+//    return "test";
+//    }
+    @GetMapping("/") //lista flimów
+    public List<Movie> getAll() {
         return movieRepository.getAll();
     }
-    @GetMapping("/movies/{id}")
-    public Movie getById(@PathVariable("id") int id){
+
+    @GetMapping("/{id}") //pojedyńczy film
+    public Movie getById(@PathVariable("id") int id) {
+
         return movieRepository.getById(id);
     }
 
-}
+    @PostMapping("/")
+    public int add(@RequestBody List<Movie> movies) {
+        return movieRepository.save(movies);
+    }
+
+    @PutMapping("/{id}") //zastępujemy cały obiekt
+    public int update(@PathVariable("id") int id, @RequestBody Movie updatedMovie) {
+        Movie movie = movieRepository.getById(id);
+        if (movie != null) {
+            movie.setName(updatedMovie.getName());
+            movie.setRating(updatedMovie.getRating());
+
+            movieRepository.update(movie);
+
+            return 1; //wszystko ok
+
+        } else {
+
+            return -1; //bład
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public int partiallyUpdate(@PathVariable("id") int id, @RequestBody Movie updatedMovie) {
+        Movie movie = movieRepository.getById(id);
+
+            if (movie != null) {
+                if (updatedMovie.getName() != null) movie.setName(updatedMovie.getName());
+                if (updatedMovie.getRating() > 0) movie.setRating(updatedMovie.getRating());
+
+                movieRepository.update(movie);
+                return 1; //wszystko ok
+
+            } else {
+
+                return -1; //bład
+            }
+        }
+        @DeleteMapping("/{id}")
+    public int delete(@PathVariable("id") int id){
+        return movieRepository.delete(id);
+        }
+    }
+
--- src/main/java/pl/pierwszastrona/movielibrary/MovieRepository.java
+++ src/main/java/pl/pierwszastrona/movielibrary/MovieRepository.java
@@ -18,6 +18,23 @@ public class MovieRepository {
     }
 
     public Movie getById(int id){
-        return jdbcTemplate.queryForObject("SELECT id, name, rating FROM movie WHERE " + "ID = ?", BeanPropertyRowMapper.newInstance(Movie.class), id);
+        return jdbcTemplate.queryForObject("SELECT id, name, rating FROM movie WHERE " + "id = ?", BeanPropertyRowMapper.newInstance(Movie.class), id);
+    }
+
+    public int save(List<Movie> movies) {
+        movies.forEach(movie -> jdbcTemplate
+                .update("INSERT INTO movie(name,rating) VALUES(?,?)",
+                        movie.getName(), movie.getRating()));
+        return 1;
+    }
+
+    public int update(Movie movie){
+        return  jdbcTemplate.update("UPDATE movie SET name=?, rating=? WHERE id=?",
+        movie.getName(), movie.getRating(), movie.getId());
+
+    }
+
+    public int delete(int id){
+        return jdbcTemplate.update("DELETE FROM movie WHERE id=?", id);
     }
 }
